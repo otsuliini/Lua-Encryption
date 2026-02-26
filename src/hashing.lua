5f8f295ccaec2a0a459e6f7fa099a0464cf16c1b
boom1401 <otso.reijonen@icloud.com>
2026-02-26 19:24:56 +0200

    Add comments for round constants and implement message schedule in chunk processing

--- src/hashing.lua
+++ src/hashing.lua
@@ -7,7 +7,7 @@ end
 
 sha256 = function(data)
 
-      local k = {
+      local k = { -- round constants
       0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
       0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
       0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -57,7 +57,17 @@ sha256 = function(data)
     end
 
     local function process_chunk(chunk)
-        for i = 1, 64
+        local w = {} -- message schedule array
+        for i = 1, 64 do
+            if i <= 16 then
+                w[i] = string.byte(chunk, (i - 1) * 4 + 1) << 24 | -- convert 4 bytes to 32-bit words
+                        string.byte(chunk, (i - 1) * 4 + 2) << 16 |
+                        string.byte(chunk, (i - 1) * 4 + 3) << 8 |
+                        string.byte(chunk, (i - 1) * 4 + 4)
+                        
+        
+        end
+        
 
     end
     
